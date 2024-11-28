ee874ff324a714fe8fb89b03b5f966c3b82cc23a
prathnow <prathnow@gmail.com>
2024-11-28 15:43:28 +0100

    Add theme provider and some theme variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,44 @@
+const theme = {
+  fontSize: {
+    large: "2rem",
+    medium: "1.5rem",
+    small: "1rem",
+  },
+  fontColor: {
+    main: "#1E1E1E",
+    secondary: "#FFFFFF",
+    tertiary: "#979797",
+    quaternary: "#C4C4C4",
+    gold: "#FF9500",
+  },
+  headingSize: {
+    h1: "4rem",
+    h2: "2rem",
+    h3: "1.2rem",
+  },
+  headingColor: {
+    h1: "#ffffff",
+    h2: "#0f0e0e",
+    h3: "#888888",
+  },
+  mainBgColor: {
+    black: "#000",
+    yellow: "#f6b509",
+    white: "#FFFFFF",
+    grey: "#2D2D2D",
+    darkGrey: "#1E1E1E",
+    lightGrey: "#FAFAFA",
+  },
+  linkColor: {
+    menuLink: "#FFFFFF",
+    menuHover: "#f6b509",
+    menuActive: "#E6A101",
+  },
+  button: {
+    primaryText: "#FFFFFF",
+    primaryBg: "linear-gradient( to left, #FFC00E, #E5A000)",
+    primaryBgHover: "#E5A000",
+  },
+};
+
+export default theme;
