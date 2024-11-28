81f5d3e8f870ba00789f76068bb37f430b32508e
prathnow <prathnow@gmail.com>
2024-11-28 15:50:48 +0100

    Add ui component: HeadingElement

---
+++ src/ui/HeadingElement.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+const tags = {
+  h1: css`
+    font-size: ${({ theme }) => theme.headingSize.h1};
+    color: ${({ theme }) => theme.headingColor.h1};
+    font-weight: 700;
+  `,
+  h2: css`
+    font-size: ${({ theme }) => theme.headingSize.h2};
+    color: ${({ theme }) => theme.headingColor.h2};
+    line-height: 1.8;
+    font-weight: 700;
+  `,
+  h3: css`
+    font-size: ${({ theme }) => theme.headingSize.h3};
+    color: ${({ theme }) => theme.headingColor.h3};
+  `,
+};
+
+const Tag = styled.div`
+  ${(props) => tags[props.$tag] || tags.h1};
+`;
+
+function HeadingElement({ tag = "h1", children }) {
+  return (
+    <Tag
+      as={tag}
+      $tag={tag}
+    >
+      {children}
+    </Tag>
+  );
+}
+
+export default HeadingElement;
