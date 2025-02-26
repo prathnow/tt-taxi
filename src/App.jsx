f12f1a0492242d3ebc28c703d770c6b93d6a8dee
prathnow <prathnow@gmail.com>
2025-02-26 18:59:56 +0100

    Add Route for Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import MainLayout from "./layouts/MainLayout";
+import Contact from "./layouts/Contact";
 
 function App() {
-  return <MainLayout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={<MainLayout />}
+        />
+        <Route
+          path="kontakt"
+          element={<Contact />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
